35c6da102e442841241c51430ba3822adb78c26d
Deepika <deepika@Deepikas-MacBook-Pro.local>
2020-10-05 19:52:13 +0530

    latest updates

--- js/main.js
+++ js/main.js
@@ -10,3 +10,27 @@ var swiper = new Swiper('.swiper-container', {
 		disableOnInteraction: false,
 	},
 });
+
+
+
+// WOw Js 
+
+wow = new WOW({
+	animateClass: 'animated',
+	offset: 100,
+	callback: function (box) {
+		console.log("WOW: animating <" + box.tagName.toLowerCase() + ">")
+	}
+});
+wow.init();
+document.getElementById('moar').onclick = function () {
+	var section = document.createElement('section');
+	section.className = 'section--purple wow fadeInDown';
+	this.parentNode.insertBefore(section, this);
+};
+
+
+// Hero slider 
+$('.carousel').carousel({
+	interval: 1000
+})
