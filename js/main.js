c90f0fa716b7a9700c4f7494e58ce883f9e42d94
Deepika <deepika@Deepikas-MacBook-Pro.local>
2020-10-17 00:14:09 +0530

    Services-Details Updates

--- js/main.js
+++ js/main.js
@@ -300,3 +300,22 @@ function detectIE() {
 	// other browser
 	return false;
 }
+//count js
+(function($) {
+		"use strict";
+		function count($this){
+		var current = parseInt($this.html(), 10);
+		current = current + 1; /* Where 50 is increment */	
+		$this.html(++current);
+			if(current > $this.data('count')){
+				$this.html($this.data('count'));
+			} else {    
+				setTimeout(function(){count($this)}, 150);
+			}
+		}        	
+		$(".stat-count").each(function() {
+		  $(this).data('count', parseInt($(this).html(), 10));
+		  $(this).html('0');
+		  count($(this));
+		});
+   })(jQuery);
